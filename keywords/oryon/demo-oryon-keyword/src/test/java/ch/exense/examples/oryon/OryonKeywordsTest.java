b76224a325ca7e4cd424902ea50dbb0fa72c704d
Jérôme Comte <jerome.comte@exense.ch>
2018-11-22 17:38:32 +0100

    Adding demo project for Oryon Keywords

---
+++ keywords/oryon/demo-oryon-keyword/src/test/java/ch/exense/examples/oryon/OryonKeywordsTest.java
@@ -0,0 +1,30 @@
+package ch.exense.examples.oryon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import step.grid.io.OutputMessage;
+import step.handlers.javahandler.KeywordRunner;
+import step.handlers.javahandler.KeywordRunner.ExecutionContext;
+
+public class OryonKeywordsTest {
+
+	ExecutionContext context;
+	
+	public void before() {
+		// Put your Keyword properties here
+		Map<String, String> properties = new HashMap<>();
+		
+		context = KeywordRunner.getExecutionContext(properties, OryonKeywords.class);
+	}
+	
+	@Test
+	public void test() throws Exception {
+		context.run("Login","{\"Username\":\"MyUser\"}");
+		OutputMessage output = context.run("Search","{\"Criteria\":\"My search criteria\"}");
+		Assert.assertEquals("My Output", output.getPayload().getString("Result"));
+	}
+}
