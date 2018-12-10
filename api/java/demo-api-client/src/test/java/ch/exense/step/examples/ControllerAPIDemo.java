78622a691b518e973cd5e46390ecbbec4f6457fb
Jérôme Comte <jerome.comte@exense.ch>
2018-12-10 14:54:35 +0100

    Adding an example for the execution of a plan existing on the controller

--- api/java/demo-api-client/src/test/java/ch/exense/step/examples/ControllerAPIDemo.java
+++ api/java/demo-api-client/src/test/java/ch/exense/step/examples/ControllerAPIDemo.java
@@ -1,8 +1,36 @@
 package ch.exense.step.examples;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+
+import step.artefacts.reports.TestCaseReportNode;
+import step.client.ControllerClient;
+
 public class ControllerAPIDemo {
 
-	public void runAnExistingPlan() {
-		
+	@Test
+	public void runAnExistingPlan() throws IOException, TimeoutException, InterruptedException {
+		try(ControllerClient client = new ControllerClient("http://step-enterprise-nightly.exense.ch", "admin", "init")) {
+			// The ID of the plan to be executed
+			String planId = "theIdOfThePlanToBeExecuted";
+			
+			// Set the execution parameters (the drop-downs that are set on the execution screen in the UI)
+			Map<String, String> executionParameters = new HashMap<>();
+			executionParameters.put("env", "TEST");
+			
+			// Execute the plan
+			String executionId = client.getExecutionManager().execute(planId, executionParameters);
+			
+			// Wait for the execution to terminate and visit the report tree
+			client.getExecutionManager().getFuture(executionId).waitForExecutionToTerminate().visitReportNodes(node->{
+				if(node instanceof TestCaseReportNode) {
+					// Do somethind....
+				}
+			});
+		}
 	}
 }
